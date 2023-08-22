a1e85a8af49f9993d21c2a76e857864849293a07
Aryan Malhotra <doctorflu@Aryans-MacBook-Air.local>
2023-08-22 09:01:10 +0530

    axes normalisation

--- heatmap/heat_maps_generation.py
+++ heatmap/heat_maps_generation.py
@@ -1,26 +1,26 @@
 import sys
 import matplotlib
+import numpy as np
 from numpy import random 
 matplotlib.use('Agg')
 
 import matplotlib.pyplot as plt
 
-x = list(range(1,11)) #if x points are not mentioned -> (1,2,3,...)
-y = random.randint(100, 300, size= 10)
-data = [[],[],[],[],[],[],[],[],[],[]]
-data1 = random.rand(12,12)
+x = np.arange(1,26) #if x points are not mentioned -> (1,2,3,...)
+y = np.arange(25,0, -1) #x & y coords
+min_val = 100
+max_val = 300
+data = random.randint(min_val, max_val, size= (25,25)  )
 
-y1 = y
-for i in y1:
-    i = i/3
 
 fig, ax = plt.subplots()
 
-heatmap = plt.imshow(data1, cmap= 'Spectral_r') #cmap = Spectral_r
+heatmap = plt.imshow(data, cmap= 'Spectral_r') #cmap = Spectral_r
 plt.title('Heatmap')
-plt.xlabel('Serial No.')
-plt.ylabel('Temprature')
+plt.xlabel('X - coord')
+plt.ylabel('Y - coord')
 cbar = plt.colorbar(heatmap)
+plt.gca().invert_yaxis()
 
 plt.savefig('/Users/doctorflu/Desktop/Thesis/Thesis-repo/heatmap/test1.png')
 plt.show()
